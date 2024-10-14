a1d37d61705df2889fee3c36189a876e62cf38e6
Alexey Kovalev <kovalev.aleksey@gmail.com>
2024-10-14 14:19:27 +0300

    replace boilerplate code with Lombok annotations

--- src/main/java/org/chiborcode/cbonds/ClientRequest.java
+++ src/main/java/org/chiborcode/cbonds/ClientRequest.java
@@ -2,17 +2,11 @@ package org.chiborcode.cbonds;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
 
+@Data
 public class ClientRequest {
     private Credentials auth;
-
-    public void setAuth(Credentials auth) {
-        this.auth = auth;
-    }
-
-    public Credentials getAuth() {
-        return this.auth;
-    }
 
     public String toJSONString() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
--- src/main/java/org/chiborcode/cbonds/Credentials.java
+++ src/main/java/org/chiborcode/cbonds/Credentials.java
@@ -1,5 +1,8 @@
 package org.chiborcode.cbonds;
 
+import lombok.Data;
+
+@Data
 class Credentials {
     private final String login;
     private final String password;
@@ -7,13 +10,5 @@ class Credentials {
     public Credentials(String login, String password) {
         this.login = login;
         this.password = password;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
     }
 }
