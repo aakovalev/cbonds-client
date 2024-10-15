5e6e8cf911152520fdf89374178a380b0321d305
Alexey Kovalev <kovalev.aleksey@gmail.com>
2024-10-15 23:25:02 +0300

    add support for client request build to create client request with given credentials and filters...

--- src/main/java/org/chiborcode/cbonds/ClientRequestBuilder.java
+++ src/main/java/org/chiborcode/cbonds/ClientRequestBuilder.java
@@ -1,18 +1,28 @@
 package org.chiborcode.cbonds;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClientRequestBuilder {
     private String user;
     private String password;
+    private final List<Filter> filters = new ArrayList<>();
 
     public ClientRequest build() {
         ClientRequest request = new ClientRequest();
         request.setAuth(new Credentials(user, password));
+        request.setFilters(filters);
         return request;
     }
 
     public ClientRequestBuilder withCredentials(String user, String password) {
         this.user = user;
         this.password = password;
+        return this;
+    }
+
+    public ClientRequestBuilder withFilter(Filter filter) {
+        filters.add(filter);
         return this;
     }
 }
---
+++ src/test/java/org/chiborcode/cbonds/ClientRequestBuilderTest.java
@@ -0,0 +1,44 @@
+package org.chiborcode.cbonds;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class ClientRequestBuilderTest {
+    private static final String USER = "user";
+    private static final String PASSWORD = "password";
+    private static final String ISIN_CODE = "ISIN_CODE";
+    private static final String EQ = "EQ";
+    private static final String APPLE_STOCK = "US0378331005";
+    private static final String ISSUED_DATE = "ISSUED_DATE";
+    private static final String GT = "GT";
+    private static final String JUN12_2020 = "2020-06-12";
+
+    @Test
+    void should_build_request_with_given_credentials() {
+        ClientRequest request = new ClientRequestBuilder()
+                .withCredentials(USER, PASSWORD)
+                .build();
+
+        Credentials actualCredentials = request.getAuth();
+        Credentials expectedCredentials = new Credentials(USER, PASSWORD);
+        assertThat(actualCredentials, equalTo(expectedCredentials));
+    }
+
+    @Test
+    void should_build_request_with_given_filters() {
+        ClientRequest request = new ClientRequestBuilder()
+                .withFilter(new Filter(ISIN_CODE, EQ, APPLE_STOCK))
+                .withFilter(new Filter(ISSUED_DATE, GT, JUN12_2020))
+                .build();
+
+        List<Filter> actualFilters = request.getFilters();
+        List<Filter> expectedFilters = List.of(
+                new Filter(ISIN_CODE, EQ, APPLE_STOCK),
+                new Filter(ISSUED_DATE, GT, JUN12_2020));
+        assertThat(actualFilters, equalTo(expectedFilters));
+    }
+}
